16af3c575b2006fd274f83f9a3d56f51f1582a66
as5589428 <as5589428@gmail.com>
2024-05-28 20:29:39 +0530

    socket added done

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "https://hardware-heaven.onrender.com/";
+const ENDPOINT = "https://socket-hardware.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
