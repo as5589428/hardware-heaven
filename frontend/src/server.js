b3bc08eddec9eb9db22dcebae0a81b3464e93f36
as5589428 <as5589428@gmail.com>
2024-05-29 15:36:56 +0530

    added server link

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://hardware-heaven.vercel.app/api/v2";
+export const server = "https://hardware-heaven-backend.vercel.app//api/v2";
 
-export const backend_url = "https://hardware-heaven.vercel.app/";
+export const backend_url = "https://hardware-heaven-backend.vercel.app//";
 
