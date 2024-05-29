eb378910cde2b6d0c961fe7c4badcddd4f46b2aa
as5589428 <as5589428@gmail.com>
2024-05-29 19:44:12 +0530

    final aman

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use("/", express.static("uploads"));
+app.use("/", express.static("uploads"));
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
