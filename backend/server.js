51e7f1b9ec7f8165990509e8e204ee3bf75839bd
as5589428 <as5589428@gmail.com>
2024-05-29 16:00:14 +0530

    added server

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,7 @@ cloudinary.config({
 // create server
 
 const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on :${process.env.PORT}`
-  );
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
 // unhandled promise rejection
