a7a9472b4a5b873cb3974a80cb7eb5122cff84ed
as5589428 <as5589428@gmail.com>
2024-06-06 21:26:10 +0530

    final changes

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const path = require("path");
+// const path = require("path");
 
 app.use(
   cors({
@@ -15,7 +15,7 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-// app.use(express.static(__dirname + "./public"));
+// app.use(express.static(__dirname, "./uploads"));
 app.use("/test", (req, res)=> {
   res.send("Hello world!");
 });
