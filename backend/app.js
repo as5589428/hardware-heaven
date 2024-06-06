f54164a7bc904ab9b80073dd58b179906cf64e2d
as5589428 <as5589428@gmail.com>
2024-06-07 01:31:03 +0530

    finasl

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/",express.static(path.join(__dirname, "./uploads")));
 app.use("/test", (req, res)=> {
-  res.send("Hello world!");
+  res.send("Hello worlds!");
 });
 
 
