2af9d47ffc4577115d799c74a24d4cb1ff4e21ce
Aman Shaikh <58882436+as5589428@users.noreply.github.com>
2024-09-23 16:00:49 +0530

    changes

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
 export const server = "https://hardware-heaven-backend.vercel.app/api/v2";
 
-export const backend_url = "https://hardware-heaven-backend.vercel.app/";
+// export const backend_url = "https://hardware-heaven-backend.vercel.app/";
 
